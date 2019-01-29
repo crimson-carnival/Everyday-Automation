9af6d331427c9e0ac695a1ee5e3726f24da63481
crimson_carnival <vyommaitreya@gmail.com>
2019-01-29 19:38:30 +0530

    Added Binary to Character converter

---
+++ Binary2Characters/Binary2Characters.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Binary2Characters {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		boolean another = true;
+		String binary;
+		int decimal, count, length;
+		char character;
+		while(another) {
+			decimal = 0;
+			count = 0;
+			System.out.print("Enter binary: ");
+			binary = sc.next();
+			length = binary.length();
+			for(int i = length-1; i>=0; i--) {
+				char c=binary.charAt(i);
+				if(c=='1') decimal+=Math.pow(2,count);
+				count++;
+			}
+			character = (char)decimal;
+			System.out.println(decimal+" "+character);
+			System.out.print("Continue? (y/n): ");
+			another = (Character.toLowerCase(sc.next().charAt(0))) == 'y';
+		}
+	}
+}
